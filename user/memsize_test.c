3b35500cd34622fd1940b2c164722cd1532a1887
Elias Assaf <elyas51000@gmail.com>
2023-03-21 14:57:37 +0200

    added memsize system call

---
+++ user/memsize_test.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(void)
+{
+  printf("Memory size of the process before calling memsize: %d\n", memsize());
+  char* tmp = malloc(20000);
+  printf("Memory size of the process after calling memsize: %d\n", memsize());
+  free(tmp); 
+  printf("Memory size of the process after the release: %d\n", memsize());
+  exit(0);
+}
