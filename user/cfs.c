2731ea61229c38a7094a5d0d1bd1c6c469944376
Elias Assaf <elyas51000@gmail.com>
2023-03-28 17:19:46 +0300

    add cfs test

---
+++ user/cfs.c
@@ -0,0 +1,57 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(void)
+{
+  int info[4];
+  int counter;
+  int counter2 = 0;
+  int pid;
+  // child 1
+  if (fork() == 0){
+    sleep(1);
+    set_cfs_priority(3);
+    for(counter = 0; counter < 10000000; counter++){
+      counter2++;
+      if(counter % 1000000 == 0){
+        sleep(10);
+      }
+    }
+    pid = getpid();
+    get_cfs_stats(pid, info);
+    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+    exit(0,"child 1 exited");
+  }
+
+  // child 2
+  if(fork() == 0){
+    sleep(2);
+    set_cfs_priority(2);
+    for(counter = 0; counter < 10000000; counter++){
+      if(counter % 1000000 == 0){
+        sleep(10);
+      }
+    }
+    pid = getpid();
+    get_cfs_stats(pid, info);
+    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+    exit(0, "child 2 exited");
+  }
+    
+    // Parent
+  set_cfs_priority(0); 
+  for (counter = 0; counter < 10000000; counter++){
+     if (counter % 1000000 == 0){
+      sleep(10);
+    }
+  }
+
+  pid = getpid();
+  get_cfs_stats(pid, info);
+  printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+  wait(0, 0);
+  wait(0, 0);
+  exit(0,"parent exited");
+}
