cfcd37de3f78a99603c09eed1c31e35fe6afd1c5
Elias Assaf <elyas51000@gmail.com>
2023-04-04 11:26:24 +0300

    print vruntime in cfs tester

--- user/cfs.c
+++ user/cfs.c
@@ -4,13 +4,14 @@
 
 #define N 1000000000
 #define M 100000000
-
+#define vruntime(p,rtime,stime,retime) (decay[p] * rtime/ (rtime+stime+retime + 1))
 int
 main(void)
 {
   int info[4];
   int counter;
   int pid;
+  int decay[3] = {75,100,125};
   // child 1
   if (fork() == 0){
     sleep(1);
@@ -22,7 +23,8 @@ main(void)
     }
     pid = getpid();
     get_cfs_stats(pid, info);
-    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
+            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
     exit(0,"child 1 exited");
   }
 
@@ -37,7 +39,8 @@ main(void)
     }
     pid = getpid();
     get_cfs_stats(pid, info);
-    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
+            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
     exit(0, "child 2 exited");
   }
     
@@ -51,7 +54,8 @@ main(void)
 
   pid = getpid();
   get_cfs_stats(pid, info);
-  printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d\n", pid, info[0], info[1], info[2], info[3]);
+    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
+            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
   wait(0, 0);
   wait(0, 0);
   exit(0,"parent exited");
