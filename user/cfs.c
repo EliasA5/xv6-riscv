4f2e43dd51c4cede6d2fe977cfe6cfec46dc631b
Elias Assaf <elyas51000@gmail.com>
2023-04-10 11:05:14 +0300

    refactor cfs into a loop

--- user/cfs.c
+++ user/cfs.c
@@ -5,6 +5,7 @@
 #define N 1000000000
 #define M 100000000
 #define vruntime(p,rtime,stime,retime) (decay[p] * rtime/ (rtime+stime+retime + 1))
+#define nchild 3
 int
 main(void)
 {
@@ -12,51 +13,25 @@ main(void)
   int counter;
   int pid;
   int decay[3] = {75,100,125};
-  // child 1
-  if (fork() == 0){
-    sleep(1);
-    set_cfs_priority(3);
-    for(counter = 0; counter < N; counter++){
-      if(counter % M == 0){
-        sleep(10);
+  int i;
+  for(i = 0; i < nchild; i++){
+    if (fork() == 0){
+      sleep(i);
+      set_cfs_priority(i%3);
+      for(counter = 0; counter < N; counter++){
+        if(counter % M == 0){
+          sleep(10);
+        }
       }
-    }
-    pid = getpid();
-    get_cfs_stats(pid, info);
-    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
-            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
-    exit(0,"child 1 exited");
-  }
-
-  // child 2
-  if(fork() == 0){
-    sleep(2);
-    set_cfs_priority(2);
-    for(counter = 0; counter < N; counter++){
-      if(counter % M == 0){
-        sleep(10);
-      }
-    }
-    pid = getpid();
-    get_cfs_stats(pid, info);
-    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
-            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
-    exit(0, "child 2 exited");
-  }
-    
-    // Parent
-  set_cfs_priority(0); 
-  for (counter = 0; counter < N; counter++){
-     if (counter % M == 0){
-      sleep(10);
+      pid = getpid();
+      get_cfs_stats(pid, info);
+      printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
+          pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
+      exit(0,"child 1 exited");
     }
   }
 
-  pid = getpid();
-  get_cfs_stats(pid, info);
-    printf("pid: %d, cfs_priority: %d, rtime: %d, stime: %d, retime: %d, vruntime: %d\n",\
-            pid, info[0], info[1], info[2], info[3], vruntime(info[0], info[1], info[2], info[3]));
-  wait(0, 0);
-  wait(0, 0);
+  for(i = 0; i < nchild; i++)
+    wait(0,0);
   exit(0,"parent exited");
 }
