2c50f60f91493c955ee711083b7c35eba665dbff
Elias Assaf <elyas51000@gmail.com>
2023-06-13 12:38:20 +0300

    add seek test

--- user/randomseek.c
+++ user/randomseek.c
@@ -4,6 +4,15 @@
 #include "kernel/fcntl.h"
 
 
+static int strncmp(const char *p, const char *q, uint n)
+{
+  while(n > 0 && *p && *p == *q)
+    n--, p++, q++;
+  if(n == 0)
+    return 0;
+  return (uchar)*p - (uchar)*q;
+}
+
 char buf1[256];
 char buf2[256];
 
@@ -39,25 +48,90 @@ test_random(int seed)
     fail = 1;
     goto ret;
   }
-//   char c;
+
   for(i = 0; i < 255; i++){
-    // c = buf1[i];
-    // printf("%d ", c);
+    // printf("%d ", buf1[i]);
     if(buf1[i] != buf2[i])
       printf("random number mismatch at idx %d\n", i);
   }
-//   printf("\n");
+  // printf("\n");
 
 ret:
+  close(random_fd);
+  return fail;
+}
+
+int test_seek()
+{
+  int fd = open("seekfile", O_RDWR | O_CREATE | O_TRUNC);
+  char buf[30] = {0};
+  int fail = 0;
+  write(fd, "hello world!", 13);
+  seek(fd, 0, SEEK_SET);
+  read(fd, buf, 13);
+  seek(fd, -13, SEEK_CUR);
+  read(fd, buf+14, 14);
+
+  if(strncmp(buf, buf+14, 13) != 0){
+    printf("SEEK TEST FAILED\n");
+    fail = 1;
+    goto ret;
+  }
+
+  buf[0] = 0;
+  seek(fd, 900, SEEK_SET);
+  seek(fd, -2, SEEK_CUR);
+  read(fd, buf, 1);
+  if(buf[0] != '!'){
+    printf("buf[0] %d, ", buf[0]);
+    printf("SEEK OUT OF BOUNDS 1 TEST FAILED\n");
+    fail = 1;
+    goto ret;
+  }
+
+  buf[0] = 0;
+  seek(fd, 900, SEEK_CUR);
+  seek(fd, -2, SEEK_CUR);
+  read(fd, buf, 1);
+  if(buf[0] != '!'){
+    printf("SEEK OUT OF BOUNDS 2 TEST FAILED\n");
+    fail = 1;
+    goto ret;
+  }
+
+  buf[0] = 0;
+  seek(fd, -100, SEEK_SET);
+  read(fd, buf, 1);
+  if(buf[0] != 'h'){
+    printf("SEEK BELOW ZERO TEST FAILED\n");
+    fail = 1;
+    goto ret;
+  }
+
+  buf[0] = 0;
+  seek(fd, -100, SEEK_CUR);
+  read(fd, buf, 1);
+  if(buf[0] != 'h'){
+    printf("SEEK BELOW ZERO TEST FAILED\n");
+    fail = 1;
+    goto ret;
+  }
+
+  ret:
+  close(fd);
   return fail;
 }
 
 int main()
 {
-  test_random(0x2A);
-//   printf("\n");
-  test_random(0x01);
+  int success = 0;
+  success |= test_random(0x2A);
+
+  success |= test_random(0x01);
   
-  printf("all tests passed\n");
-  exit(0);
+  success |= test_seek();
+
+  printf(success ? "some tests failed\n" : "all tests passed\n");
+  exit(success);
 }
+
